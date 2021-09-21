22790330a3b6452fb0e9283835e8d41a27a2b697
developerlin123 <developerlin123@gmail.com>
2021-09-21 09:43:35 +0200

    first commit

---
+++ src/components/Pos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="pos">
+      <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pos',
+  data(){
+      return {
+          msg:"haha"
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
